6f5d53830e6e4f2b8b33db84db53958004c7b63b
001osama <osamanasim909@gmail.com>
2023-10-15 20:50:06 +0500

    getting user at initiation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,21 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  userService = inject(UserService);
+
+  constructor(){
+    const user = this.userService.getUserFromStorage();
+    if(!user){
+      const randomNumber = Math.ceil(Math.random() * 4000 + 1000);
+      const randomName = `user_${randomNumber}`; 
+      this.userService.createUser(randomName)
+      .subscribe(user => {
+        console.log('user created', user);
+        this.userService.saveUserToStorage(user);
+      });
+    }
+  }
+
   title = 'threads-app';
 }
