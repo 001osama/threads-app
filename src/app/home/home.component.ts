19bd063f13a2ceee1f7569ac078e4e79823b95f5
001osama <osamanasim909@gmail.com>
2023-10-15 20:47:46 +0500

    home component logic implemented

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommentComponent } from "../components/comment/comment.component";
+import { CommentService } from '../services/comment.service';
+import { Comment } from '../interfaces/comment.interface';
+import { CommentFormComponent } from "../components/comment-form/comment-form.component";
+import { UserService } from '../service/user.service';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    imports: [CommonModule, CommentComponent, CommentFormComponent]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+    commentService = inject(CommentService);
+    userService = inject(UserService);
+    comments = signal<Comment[]>([]);
+    ngOnInit(): void{
+        this.getComments();
+    }
+
+    createComment(formValue:{text:string}){
+        const {text} = formValue;
+        const user = this.userService.getUserFromStorage();
+        if(!user)
+        return;
+        this.commentService.createComment({
+            text:text,
+            userId: user._id,
+        })
+        .subscribe((createdComment)=>{
+            this.comments.set([createdComment,...this.comments()]);
+        });
+    }
+
+    getComments(){
+        this.commentService.getComments()
+        .subscribe((comments) => this.comments.set(comments));
+    }
+
+    commentTrackBy(index:number, comment: Comment){
+        return comment._id;
+    }
 
 }
